cd670e3a402bcd745b29706d66379427afc7790b
chuccam1011 <39576740+chuccam1011@users.noreply.github.com>
2019-05-07 15:53:28 +0700

    7/7

--- app/src/main/java/com/example/ongthia1/MainActivity.java
+++ app/src/main/java/com/example/ongthia1/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         buttonChonde= (Button) findViewById(R.id.buttonThiTheoDe);
         buttonChonde.setOnClickListener(new View.OnClickListener() {
             @Override
