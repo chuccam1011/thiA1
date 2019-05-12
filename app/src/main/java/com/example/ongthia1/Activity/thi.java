0d6848cc8862cb9473b451cd97c085639383acda
HaiPTIT <haivtndc@gmail.com>
2019-05-13 02:35:32 +0700

    add view

---
+++ app/src/main/java/com/example/ongthia1/Activity/thi.java
@@ -0,0 +1,14 @@
+package com.example.ongthia1.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.ongthia1.R;
+
+public class thi extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thi);
+    }
+}
